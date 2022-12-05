8d2353c21a4e5c7e6518c2304f386976a7b4ca2e
Zalan Blenessy <zalan.blenessy@volvocars.com>
2022-12-05 11:28:37 +0100

    Ready for deployment

--- run_poc.sh
+++ run_poc.sh
@@ -1,3 +1,5 @@
-export TEMPO_TOKEN=$(cat ../tempo_token)
 docker build -t tempo_poc . 
-docker run -it -v $pwd/.ssh:/home/user/.ssh:ro --env TEMPO_TOKEN tempo_poc
+
+export TEMPO_TOKEN=$(cat ../tempo_token)
+export SSH_KEY=$(cat ~/.ssh/testing)
+docker run --rm -it -e TEMPO_TOKEN -e SSH_KEY -e SSH_KEY_TYPE=rsa tempo_poc
