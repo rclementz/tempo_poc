7abb08d57938bb7062f0c0e3eac70b0e4aa6d954
rclementz <rclemenet@volvocars.com>
2022-12-07 09:08:07 +0000

    latest

--- re_run.sh
+++ re_run.sh
@@ -1,3 +1,4 @@
 export TEMPO_TOKEN=$(cat ../tempo_token)
 export SSH_KEY=$(cat ~/.ssh/id_rsa)
-docker run --rm -it -e TEMPO_TOKEN -e SSH_KEY -e SSH_KEY_TYPE=rsa tempo_poc 
+sudo docker run --rm -it -e TEMPO_TOKEN -e SSH_KEY -e SSH_KEY_TYPE=rsa tempo_poc bash
+
