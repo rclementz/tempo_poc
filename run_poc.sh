93ce0964ba7ccfbdff91e6a6bc83135500f0014b
rclementz <rclement@volvocars.com>
2022-12-07 10:46:59 +0100

    removed old ver. bash files

--- run_poc.sh
+++
@@ -1,6 +0,0 @@
-docker build -t tempo_poc . 
-
-export TEMPO_TOKEN=$(cat ../tempo_token)
-export SSH_KEY=$(cat ~/.ssh/id_rsa)
-docker run --rm -it -e TEMPO_TOKEN -e SSH_KEY -e SSH_KEY_TYPE=rsa tempo_poc 
-
